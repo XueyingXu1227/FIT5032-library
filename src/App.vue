7d9418c72c931126849635518cfc6413f5c9c058
Xueying Xu <xxuu0125@student.monash.edu>
2025-08-18 19:22:01 +1000

    Activity 1 done

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,7 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import JSON from './components/JSON.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-    <section style="margin-top: 40px; border-top: 1px solid #ccc; padding-top: 20px">
-      <h2>JSON Lab</h2>
-      <JSON />
-    </section>
-  </main>
+  <Form />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<script setup>
+import Form from './components/Form.vue'
+</script>
