91ffa8d9ec902017a0a91770c7467d613df0bddf
Xueying Xu <xxuu0125@student.monash.edu>
2025-08-19 16:31:45 +1000

    Activity 7 done.

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -55,23 +55,24 @@
           <button type="button" class="btn btn-secondary" @click="clearForm">Clear</button>
         </form>
         <div class="row mt-5" v-if="submittedCards.length">
-          <div class="d-flex flex-wrap justify-content-start">
+          <div class="row">
             <div
               v-for="(card, index) in submittedCards"
               :key="index"
-              class="card m-2"
-              style="width: 18rem"
+              class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3"
             >
-              <div class="card-header">User Information</div>
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">Username: {{ card.username }}</li>
-                <li class="list-group-item">Password: {{ card.password }}</li>
-                <li class="list-group-item">
-                  Australian Resident: {{ card.isAustralian ? 'Yes' : 'No' }}
-                </li>
-                <li class="list-group-item">Gender: {{ card.gender }}</li>
-                <li class="list-group-item">Reason: {{ card.reason }}</li>
-              </ul>
+              <div class="card" style="width: 100%">
+                <div class="card-header">User Information</div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">Username: {{ card.username }}</li>
+                  <li class="list-group-item">Password: {{ card.password }}</li>
+                  <li class="list-group-item">
+                    Australian Resident: {{ card.isAustralian ? 'Yes' : 'No' }}
+                  </li>
+                  <li class="list-group-item">Gender: {{ card.gender }}</li>
+                  <li class="list-group-item">Reason: {{ card.reason }}</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
